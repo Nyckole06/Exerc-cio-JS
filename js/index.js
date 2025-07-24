36495f8216dcbc3ad07ed5fb2a1b58ed499703a2
Nyckole06 <nyck.cursos@gmail.com>
2025-07-23 21:57:53 -0300

    alterações na tarefa

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function Entrar()
+{
+    const usuario = document.getElementById("usuario").value;
+
+    const senha = document.getElementById("senha").value;
+
+    const mensagem = document.getElementById("mensagem");
+    
+    const emailSalvo = localStorage.getItem("emailCadastrado");
+
+    const senhaSalva = localStorage.getItem("senhaCadastrada");
+
+    
+    
+
+    console.log(emailSalvo)
+
+    if(usuario === "" || senha === "")
+    {
+        mensagem.textContent = "Por favor, preencha todos os campos!";
+        mensagem.className = "erro";
+        return;
+        
+    }
+
+    if (usuario === emailSalvo && senha === senhaSalva)
+    {
+        mensagem.textContent = "Cadastrado com sucesso!";
+        mensagem.className = "sucesso";
+
+        window.location.href="select.html";
+    }
+    else
+    {
+        mensagem.textContent = "E-mail ou senha inválidos. Tente novamente!";
+        mensagem.className = "erro";
+    }
+
+
+}
